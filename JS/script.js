da705f0b5a5626e6611b0a0fb442beabe50e7b2b
Dmytro Zeliak <105452303+Dimuello@users.noreply.github.com>
2022-11-12 16:43:54 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        arrows: true,
+        centerMode: true,
+        infinite: true,
+        speed: 400,
+        slidesToShow: 1,
+    });
+});
